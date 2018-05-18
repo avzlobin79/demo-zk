a0b81c4e1846627a9c6fe79240f30f83e9e7eb24
Alex <avzlobin79@gmail.com>
2018-05-18 18:57:18 +0300

    first commit

---
+++ src/main/java/org/zkoss/essentials/chapter3/mvc/ProfileViewController.java
@@ -0,0 +1,112 @@
+/* 
+	Description:
+		ZK Essentials
+	History:
+		Created by dennis
+
+Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+*/
+package org.zkoss.essentials.chapter3.mvc;
+
+import java.util.Set;
+
+import org.zkoss.essentials.entity.User;
+import org.zkoss.essentials.services.*;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zkplus.spring.DelegatingVariableResolver;
+import org.zkoss.zul.*;
+
+@VariableResolver(DelegatingVariableResolver.class)
+public class ProfileViewController extends SelectorComposer<Component>{
+	private static final long serialVersionUID = 1L;
+
+	//wire components
+	@Wire
+	Label account;
+	@Wire
+	Textbox fullName;
+	@Wire
+	Textbox email;
+	@Wire
+	Datebox birthday;
+	@Wire
+	Listbox country;
+	@Wire
+	Textbox bio;
+	@Wire
+	Label nameLabel;
+	
+	//wire services
+	@WireVariable
+	AuthenticationService authService;
+	@WireVariable
+	UserInfoService userInfoService;
+	
+	@Override
+	public void doAfterCompose(Component comp) throws Exception{
+		super.doAfterCompose(comp);
+		
+		ListModelList<String> countryModel = new ListModelList<String>(CommonInfoService.getCountryList());
+		country.setModel(countryModel);
+		
+		refreshProfileView();
+	}
+	
+	
+	@Listen("onClick=#saveProfile")
+	public void doSaveProfile(){
+		UserCredential userCredential = authService.getUserCredential();
+		User user = userInfoService.findUser(userCredential.getAccount());
+		if(user==null){
+			//TODO handle un-authenticated access 
+			return;
+		}
+		
+		//apply component value to bean
+		user.setFullName(fullName.getValue());
+		user.setEmail(email.getValue());
+		user.setBirthday(birthday.getValue());
+		user.setBio(bio.getValue());
+		
+		Set<String> selection = ((ListModelList)country.getModel()).getSelection();
+		if(!selection.isEmpty()){
+			user.setCountry(selection.iterator().next());
+		}else{
+			user.setCountry(null);
+		}
+		
+		nameLabel.setValue(fullName.getValue());
+		
+		userInfoService.updateUser(user);
+		
+		Clients.showNotification("Your profile is updated");
+	}
+	
+	@Listen("onClick = button[label = 'Reload']")
+	public void doReloadProfile(){
+		refreshProfileView();
+	}
+
+	private void refreshProfileView() {
+		UserCredential cre = authService.getUserCredential();
+		User user = userInfoService.findUser(cre.getAccount());
+		if(user==null){
+			//TODO handle un-authenticated access 
+			return;
+		}
+		
+		//apply bean value to UI components
+		account.setValue(user.getAccount());
+		fullName.setValue(user.getFullName());
+		email.setValue(user.getEmail());
+		birthday.setValue(user.getBirthday());
+		bio.setValue(user.getBio());
+		
+		((ListModelList)country.getModel()).addToSelection(user.getCountry());
+
+		nameLabel.setValue(user.getFullName());
+	}
+}
